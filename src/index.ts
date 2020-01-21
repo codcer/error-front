40ec1b0f6deb473fbdb5db1d1b4c8da0e11382f6
cof <fanliu@mistong.com>
2020-01-21 17:26:00 +0800

    feat: test ts code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-const Cc = 1;
+const CC = 1;
 const ccFn = () => 1;
 
 const arr = [1, 2, 3, 5, 6];
 
-const a = arr.map(r => r * 2);
-const b = Cc && arr && ccFn && Cc && arr && ccFn && Object.keys({ a: 1, b: 3 });
-console.log("dddd");
+const a = arr.map((r) => r * 2);
+const b = CC && arr && ccFn && CC && arr && ccFn && Object.keys({ a: 1, b: 3 });
+
+console.log('dddd', 'dddsxxx');
